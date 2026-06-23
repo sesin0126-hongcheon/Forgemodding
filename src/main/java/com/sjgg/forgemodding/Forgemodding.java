a6e8d72933f037db083d37cdb701a4fbd9451b9c
SJ-gg <sesin0126.kang@gmail.com>
2026-06-24 00:05:23 +0900

    added Sonic_gun (without texture)

--- src/main/java/com/sjgg/forgemodding/Forgemodding.java
+++ src/main/java/com/sjgg/forgemodding/Forgemodding.java
@@ -1,6 +1,7 @@
 package com.sjgg.forgemodding;
 
 import com.mojang.logging.LogUtils;
+import com.sjgg.forgemodding.item.Sonic_gun;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.food.FoodProperties;
@@ -32,103 +33,33 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Forgemodding.MODID)
 public class Forgemodding {
-
-    // Define mod id in a common place for everything to reference
+    // MODID, 로거( 절대 건들지 말도록..) ------------------------------------------
     public static final String MODID = "forgemodding";
-
-    // 로거(기록, 건들지 말도록..) ------------------------------------------------------------------------
-
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    // 블록 & 아이템 추가------------------------------------------------------------------------
-
-    // Create a Deferred Register to hold Blocks which will all be registered under the "forgemodding" namespace
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "forgemodding" namespace
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "forgemodding" namespace
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
-
-    // Creates a new Block with the id "forgemodding:example_block", combining the namespace and path
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
-    // Creates a new BlockItem with the id "forgemodding:example_block", combining the namespace and path
-    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
-
-    // 음식 추가 ------------------------------------------------------------------------
-
-    // Creates a new food item with the id "forgemodding:example_id", nutrition 1 and saturation 2
-    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().alwaysEat().nutrition(1).saturationMod(2f).build())));
-
-    // 아이템 탭 ------------------------------------------------------------------------
-
-    // Creates a creative tab with the id "forgemodding:example_tab" for the example item, that is placed after the combat tab
-    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder().withTabsBefore(CreativeModeTabs.COMBAT).icon(() -> EXAMPLE_ITEM.get().getDefaultInstance()).displayItems((parameters, output) -> {
-        output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-    }).build());
-
-    // 메인 ------------------------------------------------------------------------
+    // 메인 --------------------------------------------------------------------
 
     public Forgemodding() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the commonSetup method for modloading
+        Sonic_gun.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
-        CREATIVE_MODE_TABS.register(modEventBus);
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
+    // ------------------------------------------------------------------------
+
     private void commonSetup(final FMLCommonSetupEvent event) {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
 
-        if (Config.logDirtBlock) LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-
-        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-
-        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) event.accept(EXAMPLE_BLOCK_ITEM);
-    }
-
-    // 서버 (사용 안할 예정)------------------------------------------------------------------------
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // 클라이언트 ------------------------------------------------------------------------
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(Sonic_gun.SONIC_GUN);
         }
     }
 }
--- src/main/java/com/sjgg/forgemodding/item/Sonic_gun.java
+++ src/main/java/com/sjgg/forgemodding/item/Sonic_gun.java
@@ -1,9 +1,20 @@
 package com.sjgg.forgemodding.item;
 
+import com.sjgg.forgemodding.Forgemodding;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
-public class Sonic_gun extends Item {
-    public Sonic_gun(Properties properties) {
-        super(properties);
+public class Sonic_gun {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Forgemodding.MODID);
+
+    public static final RegistryObject<Item> SONIC_GUN = ITEMS.register("sonic_gun",
+            () -> new Item(new Item.Properties()));
+
+    public  static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
     }
 }
